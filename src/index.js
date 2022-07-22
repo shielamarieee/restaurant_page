8e76375f69d11a7d3322c0b6cebfa56d6f518133
Shiela <sheshemarie31@gmail.com>
2022-07-22 15:10:22 +0800

    Add switching tab logic

--- src/index.js
+++ src/index.js
@@ -1,4 +1,53 @@
 import initialPage from "./initialPage";
-import './style.css';
+import "./style.css";
+import homePage from "./homePage";
+import menuPage from "./menuPage";
+import contactPage from "./contactPage";
 
 initialPage();
+
+//switch tab logic
+let main = document.querySelector('.main');
+
+//wipe out contents
+const removeChilds = (parent) => {
+  while(parent.lastChild) {
+    parent.removeChild(parent.lastChild);
+  }
+}
+
+const homeTab = document.querySelector('.home');
+homeTab.addEventListener('click', (e) => {
+  removeChilds(main);
+  homePage();
+  if (e.target.classList.contains("active")) return;
+    setActiveButton(homeTab);
+  console.log('1');
+});
+
+const orderBtn = document.querySelector('.order-btn');
+orderBtn.addEventListener('click', (e) => {
+  removeChilds(main);
+  menuPage();
+  if (e.target.classList.contains("active")) return;
+    setActiveButton(menuTab);
+  console.log('2')
+})
+
+const menuTab = document.querySelector('.menu');
+menuTab.addEventListener('click', (e) => {
+  removeChilds(main);
+  menuPage();
+  if (e.target.classList.contains("active")) return;
+    setActiveButton(menuTab);
+  console.log('3');
+});
+
+const contactTab = document.querySelector('.contact');
+contactTab.addEventListener('click', (e) => {
+  removeChilds(main);
+  contactPage();
+  if (e.target.classList.contains("active")) return;
+    setActiveButton(contactTab);
+  console.log('4');
+});
